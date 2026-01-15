e693883fafa2af345a5267237888f5660eb414d8
onulee <onulee@naver.com>
2026-01-15 13:17:55 +0900

    update

--- sjpt13/src/pages/BList.js
+++ sjpt13/src/pages/BList.js
@@ -1,18 +1,74 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const BList = () => {
+
+    const navigate = useNavigate();
+    const [flag,setFlag] = useState(false);
+    const [boards,setBoards] = useState(
+        [
+            // {'bno':'1','btitle':'게시글 제목입니다.1','bcontent':'게시글 내용입니다.1'},
+            // {'bno':'2','btitle':'게시글 제목입니다.2','bcontent':'게시글 내용입니다.2'},
+            // {'bno':'3','btitle':'게시글 제목입니다.3','bcontent':'게시글 내용입니다.3'},
+        ]
+    );
+
+    useEffect(
+        () =>{
+            //axios -> setBoards() : 파라미터로 전달 params추가
+            axios.get('http://localhost:8000/customer/clistJson/',
+                {params:{'id':'aaa','page':1}}
+            )
+            .then(res=>{
+                console.log("넘어온 데이터 list",res.data.list);
+                setBoards(res.data.list);
+            })
+        },[flag]
+    )
+
+    const deleteBtn = (bno) =>{
+        if(window.confirm(`${bno}번 게시글을 삭제하시겠습니까?`)){
+            axios.delete(`http://localhost:8000/customer/cdeleteJson/${bno}/`,{data:{'name':'홍길동'}})
+            .then(res=>{
+                alert(`${bno}번 게시글을 삭제했습니다.`);
+                navigate('/BList');
+                setFlag(!flag);
+            })
+        }
+    }
+
+    const updateBtn = (bno) =>{
+        navigate('/BWrite',{state:{'bno':bno}})
+    }
+
+    //게시판리스트 출력함수 - map사용시 key입력
+    const board_list = boards.map( (board) => {
+        return(
+            <div class="card" key={board.bno}>
+                <h5 class="card-header">번호 : {board.bno}</h5>
+                <div class="card-body">
+                    <h5 class="card-title">제목 : {board.btitle}</h5>
+                    <p class="card-text">{board.bcontent}</p>
+                    <button onClick={()=>updateBtn(board.bno)} class="btn btn-primary">수정</button>
+                    <button onClick={()=>deleteBtn(board.bno)} class="btn btn-primary">삭제</button>
+                </div>
+            </div>
+        )
+    })
+
+    const writeBtn = () =>{
+        navigate('/BWrite')
+    }
+
     return(
         <div className="root">
             <h2>BList 게시판리스트</h2>
-
-            <div class="card">
-                <h5 class="card-header">Featured</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button onClick={writeBtn} class="btn btn-primary me-md-2" type="button">글쓰기</button>
             </div>
+            {board_list}
+            
 
             
 
--- sjpt13/src/pages/BWrite.js
+++ sjpt13/src/pages/BWrite.js
@@ -1,20 +1,73 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import { useNavigate,useLocation } from "react-router-dom";
+import axios from "axios";
 
 const BWrite = () => {
+    const location = useLocation(); // 선언
+    const {bno} = location.state || {};
+
+    const navigate = useNavigate();
+    const [btitle,setBtitle] = useState('');
+    const [bcontent,setBcontent] = useState('');
+    const [id,setId] = useState('aaa'); //로그인을 적용해서 session,token id를 가져옴.
+
+    
+
+    // 글수정
+    useEffect(
+        () =>{
+           //axios -> setBoards() : 파라미터로 전달 params추가
+           if(bno){
+               axios.get(`http://localhost:8000/customer/cviewJson/${bno}/`,
+                    {params:{'id':'aaa','page':1}}
+                )
+                .then(res=>{
+                    console.log("넘어온 데이터 board : ",res.data.board);
+                    console.log("넘어온 데이터 member_id : ",res.data.board.member_id);
+                    setBtitle(res.data.board.btitle);
+                    setBcontent(res.data.board.bcontent);
+                    setId(res.data.board.member_id);
+                })
+           }else{
+            setBtitle('');
+            setBcontent('');
+            
+           } 
+        },[bno]
+    )
+
+    const saveBtn = () =>{
+        //axios -> 게시글저장 : post방식 {}객체형태로 전송
+        if(bno){
+            axios.put('http://localhost:8000/customer/cupdateJson/',{'bno':bno,'id':id,'btitle':btitle,'bcontent':bcontent})
+            .then(res=>{
+                alert('게시글을 저장합니다.');
+                navigate('/BList');
+            })
+        }else{
+            axios.post('http://localhost:8000/customer/cwriteJson/',{'id':id,'btitle':btitle,'bcontent':bcontent})
+            .then(res=>{
+                alert('게시글을 저장합니다.');
+                navigate('/BList');
+            })
+        }
+    }
+
+
     return(
         <div className="root">
-          <h2>BWrite 게시글등록</h2>
+          <h2>{bno?'게시글수정':'BWrite 게시글등록'}</h2>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">게시글 제목</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="제목을 입력하세요."/>
+            <label htmlFor="exampleFormControlInput1" class="form-label">게시글 제목</label>
+            <input type="text" value={btitle} onChange={(e)=>setBtitle(e.target.value)} class="form-control" id="exampleFormControlInput1" placeholder="제목을 입력하세요."/>
             </div>
             <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">내용</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label htmlFor="exampleFormControlTextarea1" class="form-label">내용</label>
+            <textarea value={bcontent} onChange={(e)=>setBcontent(e.target.value)} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
           </div>
           <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-primary me-md-2" type="button">Button</button>
-            <button class="btn btn-primary" type="button">Button</button>
+            <button onClick={saveBtn} class="btn btn-primary me-md-2" type="button">{bno?'수정':'저장'}</button>
+            <button class="btn btn-primary" type="button">취소</button>
           </div>
         </div>
     )
